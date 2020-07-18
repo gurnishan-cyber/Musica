90b3d5bd5abc240a0816ae18dcd25b3989884a94
gurnishan <64218456+gurnishan-cyber@users.noreply.github.com>
2020-07-18 16:11:48 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "slotify");
+	$con = mysqli_connect("127.0.0.1", "root", "", "slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
