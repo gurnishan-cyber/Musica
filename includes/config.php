24f84bfe41923317cbff6c907b03862546866ccf
gurnishan <64218456+gurnishan-cyber@users.noreply.github.com>
2020-07-18 16:14:51 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("127.0.0.1", "root", "", "slotify");
+	$con = mysqli_connect("ec2-3-208-50-226.compute-1.amazonaws.com", "pxrbjxcvessmbp", "6c16d6f3d16cf0c3ef865880019f39755859fe0bd9bab37febffffe32a4d5866", "");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
